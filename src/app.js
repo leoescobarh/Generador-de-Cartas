5cc2d1ed81a82591acb6a5f33c6a8276ec625cd9
leoescobarh <ldescobarherrera@gmail.com>
2022-06-25 02:27:08 +0000

    avance de generador de cartas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#abajote , .arriba").innerHTML = generarSimbolo();
+  document.querySelector(".numero").innerHTML = generarNumero();
+};
+
+let generarNumero = () => {
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numerotes = Math.floor(Math.random() * numeros.length);
+  return numeros[numerotes];
+};
+
+let generarSimbolo = () => {
+  let simbolos = ["♦", "♥", "♠", "♣"];
+  let simbolotes = Math.floor(Math.random() * simbolos.length);
+  return simbolos[simbolotes];
 };
